c2611317297b439888abec2260d438700fb8a0ba
lhk3337 <lhk3337@naver.com>
2021-06-16 20:19:38 +0900

    interface type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "Holim",
+  age: 24,
+  gender: "male",
 };
-console.log(sayHi("Holime", 24, "male"));
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+};
+console.log(sayHi(person));
 export {};
