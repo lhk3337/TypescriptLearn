1bcbe1d1358354480c6587f823a0644fb0bccf98
lhk3337 <lhk3337@naver.com>
2021-06-16 18:37:08 +0900

    parameter and argument type 선언

--- index.ts
+++ index.ts
@@ -1,9 +1,5 @@
-const name = "holim",
-  age = 24,
-  gender = "male";
-
-const sayHi = (name, age, gender?) => {
+const sayHi = (name: string, age: number, gender: string) => {
   console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
 };
-sayHi(name, age);
+sayHi("Holime", 24, "male");
 export {};
