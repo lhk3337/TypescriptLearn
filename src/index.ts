28e57a846e5bee589d1fa9cdee7fdf30763e1343
lhk3337 <lhk3337@naver.com>
2021-06-18 23:56:18 +0900

    Typescript에서 Classes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "Holim",
-  age: 24,
-  gender: "male",
-};
+const person = { name: "holim", age: 22, gender: "male" };
 
 const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
-console.log(sayHi(person));
+
+console.log(person);
 export {};
