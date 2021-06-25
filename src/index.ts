681c075ec0992550901e998c8bbea93b6a19575d
lhk3337 <lhk3337@naver.com>
2021-06-25 12:52:01 +0900

    create block chain1

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
-
-const person = new Human("Holim", 20, "male");
-
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
-
-console.log(sayHi(person));
+const genesisBlock: Block = new Block(0, "201010", "", "good", 23);
+console.log(genesisBlock);
+let blockchein: [Block] = [genesisBlock];
+console.log(blockchein);
 export {};
